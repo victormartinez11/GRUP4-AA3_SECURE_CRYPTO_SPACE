c56acd9390086ebf5a31e1d6289404625d8f2d8a
arnausarri <arnau.sarri@estudiant.enti.cat>
2025-12-17 17:11:44 +0100

    Create crypto_module.py

---
+++ crypto_module.py
@@ -0,0 +1,12 @@
+"""
+Crypto Module
+
+This module handles encryptation and descryption operations using symestric cryptography.4
+
+Responsabilities:
+- Key generations
+- File encryptation
+- File descryption
+
+Cryptographic operation will follow secure standards.
+"""
