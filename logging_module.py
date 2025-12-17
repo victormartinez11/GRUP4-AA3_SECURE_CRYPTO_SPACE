86e46f52fd5419639faab8f7bce950d53c91dc67
arnausarri <arnau.sarri@estudiant.enti.cat>
2025-12-17 17:17:52 +0100

    Create logging_module.py

---
+++ logging_module.py
@@ -0,0 +1,10 @@
+"""
+Logging Module
+This module records all actions performed inside Secure Crypto Space.
+
+Responsabilities:
+- User activity tracking
+- File access logging
+- Audit trail generation
+
+Logs are stored for security and traceability
