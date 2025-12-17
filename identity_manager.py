e6f39a213b48248d542aff8a3b69fbc4feea1098
arnausarri <arnau.sarri@estudiant.enti.cat>
2025-12-17 17:08:59 +0100

    Update identity_manager.py

--- identity_manager.py
+++ identity_manager.py
@@ -1,1 +1,12 @@
+"""
+Identify Manager Module
 
+This module is responsible for managing users and access control within Secure Crypto Space.
+
+Responsabilities:
+- User creation
+- Authentication
+- Access isolation between users
+
+Implementation will be added in future versions.
+"""
