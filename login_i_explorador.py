a97588f7299f473ae052113b3c83594c1a82a471
MarcLahiguera <marc.lahiguera@estudiant.enti.cat>
2025-12-21 11:25:01 +0100

    Implementació del botó login per a que sigui funcional i et porti al explorador d'arxius.

---
+++ login_i_explorador.py
@@ -0,0 +1,113 @@
+import customtkinter as ctk
+import os
+from src.dashboard import dashboard 
+
+app = ctk.CTk()
+app.title("Secure Vault Login")
+app.geometry("400x400") 
+
+
+def guardar_usuari():
+    usuari = new_user_entry.get()
+    contra = new_pass_entry.get()
+
+    if usuari == "" or contra == "":
+        etiqueta_missatge.configure(text="Has de omplir tots els camps!", text_color="red")
+        return
+
+    try:
+        with open("registre.txt", "a") as fitxer: # 'a' per afegir
+            fitxer.write(f"{usuari},{contra}\n")
+        
+        etiqueta_missatge.configure(text="Saved successfully!", text_color="green")
+    except Exception as e:
+        etiqueta_missatge.configure(text=f"Error: {e}", text_color="red")
+
+def verificar_usuari():
+    usuari = entry.get()
+    contra = password_entry.get()
+    login_correcte = False
+
+    try:
+        if not os.path.exists("registre.txt"):
+            etiqueta_login.configure(text="No hi ha usuaris registrats", text_color="orange")
+            return
+
+        with open("registre.txt","r") as fitxer:
+            for linia in fitxer:
+                dades = linia.strip().split(",")
+                if len(dades) == 2:
+                    if dades[0] == usuari and dades[1] == contra:
+                        login_correcte = True
+                        break 
+
+        if login_correcte:
+            etiqueta_login.configure(text="Login Correcte!", text_color="green")
+            print("Login acceptat. Carregant dashboard...")
+            app.after(500, carregar_dashboard) 
+            
+        else:
+            etiqueta_login.configure(text="Usuari o contrasenya incorrectes", text_color="red")
+
+    except Exception as e:
+        etiqueta_login.configure(text=f"Error al llegir: {e}", text_color="red")
+
+def show_register():
+    login_frame.pack_forget()
+    register_frame.pack(fill="both", expand=True, pady=20)
+
+def show_login():
+    register_frame.pack_forget()
+    login_frame.pack(fill="both", expand=True, pady=20)
+
+
+login_frame = ctk.CTkFrame(app, fg_color="transparent")
+register_frame = ctk.CTkFrame(app, fg_color="transparent")
+
+
+ctk.CTkLabel(login_frame, text="Login", font=("Arial", 20)).pack(pady=10)
+
+entry = ctk.CTkEntry(login_frame, placeholder_text="Username", width=200)
+entry.pack(pady=10)
+
+password_entry = ctk.CTkEntry(login_frame, placeholder_text="Password", show="*", width=200)
+password_entry.pack(pady=10)
+
+etiqueta_login = ctk.CTkLabel(login_frame, text="", font=("Arial", 12))
+etiqueta_login.pack(pady=5)
+
+ctk.CTkButton(login_frame, text="Login", command=verificar_usuari).pack(pady=10)
+ctk.CTkLabel(login_frame, text="Don't have an account?", width=200).pack(pady=(20,5))
+ctk.CTkButton(login_frame, text="Register", command=show_register).pack(pady=5)
+
+ctk.CTkLabel(register_frame, text="Create Account", font=("Arial", 20)).pack(pady=10)
+
+new_user_entry = ctk.CTkEntry(register_frame, placeholder_text="New Username", width=200)
+new_user_entry.pack(pady=10)
+
+new_pass_entry = ctk.CTkEntry(register_frame, placeholder_text="New Password", show="*", width=200)
+new_pass_entry.pack(pady=10)
+
+etiqueta_missatge = ctk.CTkLabel(register_frame, text="", font=("Arial", 12))
+etiqueta_missatge.pack(pady=5)
+
+ctk.CTkButton(register_frame, text="Sign Up", fg_color="green", command=guardar_usuari).pack(pady=20)
+ctk.CTkButton(register_frame, text="Back to Login", fg_color="gray", command=show_login).pack(pady=5)
+
+
+login_frame.pack(fill="both", expand=True, pady=20)
+
+
+def carregar_dashboard():
+# Eliminem completament els frames del Login
+    login_frame.destroy()
+    register_frame.destroy()
+    
+
+    app.geometry("950x650")
+    app.title("Explorador de Fitxers")
+    
+
+    dashboard(app)
+
+app.mainloop()
