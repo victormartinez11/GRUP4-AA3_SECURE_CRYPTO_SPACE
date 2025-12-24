1c2511015567be0e73be4ca72995bbf5cd7e3f89
pacollamas <francisco.llamas@estudiant.enti.cat>
2025-12-24 15:59:14 +0100

    Importaedor fitxers

--- src/importfile.py
+++ src/importfile.py
@@ -4,15 +4,19 @@ import os
 
 def accio_importar():
     try:
+        #Open windows explorer
         origin= filedialog.askopenfilename(
                 title="Import to vault",
                 filetypes=(("All files", "*.*"), ("Text files", "*.txt"), ("Image files", "*.png;*.jpg"))
             )
-        if not origin:
-            return
-        destination = os.getcwd()
-        shutil.copy(origin, destination)
-        print(f"File imported: {origin}")
+        if origin:
+            #Copy file
+            destination = os.getcwd()
+            shutil.copy(origin, destination)
+            print(f"File imported: {origin}")
+        else:
+            raise Exception("No file selected")
     except Exception as e:
         print(f"[ERROR]: {e}")
       
+
