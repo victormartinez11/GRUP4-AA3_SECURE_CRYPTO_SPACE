04472f055709dc0bda40ecdb4e3b14372b7e569e
Sheng Ling <shengling.xu@estudiant.enti.cat>
2025-12-24 20:22:24 +0100

    Creación del crear vault
    Creación del codigo para crear el vault nuevo

---
+++ crear_nou_vault.py
@@ -0,0 +1,54 @@
+# -*- coding: latin-1 -*-
+
+import os
+import hashlib
+import shutil
+
+BASE_DIR = "storage"
+VAULT = os.path.join(BASE_DIR, "Vault")
+PASSWORD_FILE = os.path.join(BASE_DIR, "password.txt")
+
+def create_vault():
+    if not os.path.exists(VAULT):
+        os.makedirs(VAULT)
+
+def set_password(password):
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    with open(PASSWORD_FILE, "w") as f:
+        f.write(hashed)
+
+def check_password(password):
+    if not os.path.exists(PASSWORD_FILE):
+        return False
+
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    with open(PASSWORD_FILE, "r") as f:
+        return f.read() == hashed
+
+def import_file(file_path, password):
+    if not check_password(password):
+        print("Contraseña incorrecta.")
+        return
+
+    if not os.path.exists(file_path):
+        print("El archivo no existe.")
+        return
+
+    vault = "storage/vault"
+    os.makedirs(vault, exist_ok=True)
+
+    file_name = os.path.basename(file_path)
+    destination = os.path.join(vault, file_name)
+
+    try:
+        shutil.copy(file_path, destination)
+
+        os.remove(file_path)
+
+        print("Archivo importado exitosamente")
+        print("Se ha borrado el archivo en su ubicación anterior")
+
+    except PermissionError:
+        print("Error: No tenemos permisos para modificar el archivo")
+    except OSError:
+        print("Error del sistema.")
